b4fbf5d46dd5ab66728085d69c99213f72ca9d8b
Harrison McGuire <hmcguire13@gmail.com>
2016-06-20 07:20:56 -0700

    Initial Commit

---
+++ custom nav barTests/custom_nav_barTests.swift
@@ -0,0 +1,36 @@
+//
+//  custom_nav_barTests.swift
+//  custom nav barTests
+//
+//  Created by Harrison McGuire on 6/20/16.
+//  Copyright © 2016 severallevels. All rights reserved.
+//
+
+import XCTest
+@testable import custom_nav_bar
+
+class custom_nav_barTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
