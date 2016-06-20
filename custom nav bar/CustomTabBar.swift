dc33afd04be6573236f631a9c0155d05083d9307
Harrison McGuire <hmcguire13@gmail.com>
2016-06-20 15:27:27 -0700

    first step complete commit

---
+++ custom nav bar/CustomTabBar.swift
@@ -0,0 +1,9 @@
+//
+//  CustomTabBar.swift
+//  custom nav bar
+//
+//  Created by Harrison McGuire on 6/20/16.
+//  Copyright © 2016 severallevels. All rights reserved.
+//
+
+import Foundation
--- custom nav bar/CustomTabBarViewController.swift
+++ custom nav bar/CustomTabBarViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomeTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
